c43ad1ec85ba17ca9dc371c744998eb28f9322a6
dileepraghumajji88 <dileepraghumajji88@gmail.com>
2024-03-06 22:30:39 +0530

    commit

--- libs/shared-services/src/common-axios-service-prs.ts
+++ libs/shared-services/src/common-axios-service-prs.ts
@@ -43,45 +43,6 @@ export class CommonAxiosServicePms {
         })
     }
 
-async axiosMailPostCall(urlEndPoint: string | {urlEndPoint: string, serviceUrl: string}, data?: any, config?: AxiosRequestConfig) {
-    let hostIp = '';
-    let queryString = '';
-    // if only query string(urlEndPoint) is specified, then the default connection host is assigned
-    if (typeof urlEndPoint == 'string') {
-        hostIp = appConfig.MAIL_URL ;
-        queryString = urlEndPoint;
-    } else {
-        // if a specific sub-service is specified then the connection host is dynamic based on the service name
-        hostIp = urlEndPoint.serviceUrl;
-        queryString = urlEndPoint.urlEndPoint;
-    }
-    return await AxiosInstance.post(hostIp + '' + queryString, data, config)
-    .then(res => {
-        return res.data;
-    }).catch(err => {
-        throw new Error(err.message);
-    })
-}
-
-async axiosMailGetCall(urlEndPoint: string | {urlEndPoint: string, serviceUrl: string}, data?: any, config?: AxiosRequestConfig) {
-    let hostIp = '';
-    let queryString = '';
-    // if only query string(urlEndPoint) is specified, then the default connection host is assigned
-    if (typeof urlEndPoint == 'string') {
-        hostIp = appConfig.MAIL_URL;
-        queryString = urlEndPoint;
-    } else {
-        // if a specific sub-service is specified then the connection host is dynamic based on the service name
-        hostIp = urlEndPoint.serviceUrl;
-        queryString = urlEndPoint.urlEndPoint;
-    }
-    return await AxiosInstance.get(hostIp + '' + queryString, data)
-    .then(res => {
-        return res.data;
-    }).catch(err => {
-        throw new Error(err.message);
-    })
-}
 }
 
     
