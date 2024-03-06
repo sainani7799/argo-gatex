0a03de11abe15db46bc1b26e0c5c156d56eda256
dileepraghumajji88 <dileepraghumajji88@gmail.com>
2024-03-06 22:32:10 +0530

    commit

--- libs/shared-services/src/email/email.service.ts
+++ libs/shared-services/src/email/email.service.ts
@@ -2,15 +2,10 @@ import { DcEmailModel } from "libs/shared-models";
 import { CommonAxiosServicePms } from "../common-axios-service-prs";
 
 export class EmailService extends CommonAxiosServicePms {
-    private Mail = '/mailer';
     private dcController = '/dc';
     
     async sendDcMail(req: DcEmailModel) {
         return await this.axiosPostCall(this.dcController + '/sendDcMail', req)
     }
     
-    async updateMailStatus(slNo: any, ) {
-        return await this.axiosMailPostCall(`updateMailStatus?slNo=${slNo}`)
-        
-    }
 }
