dff15e5120b9970840fe5510743456dd18e27a2b
Meghana Tammina <meghanatammina.ce@gmail.com>
2024-10-23 12:14:12 +0000

    Update config.ts

--- libs/shared-services/config.ts
+++ libs/shared-services/config.ts
@@ -1,6 +1,6 @@
-const mailLiveURL = "http://localhost:3005/api/mailer";
+const mailLiveURL = "http://139.59.79.77:3338/api/mailer";
 
 export const config = {
-    APP_DC: "http://localhost:3011/api",
+    APP_DC: "http://139.59.79.77:3338/api",
     // APP_DC: "http://gpdc.seplcloud.com:3011/api",
 }
