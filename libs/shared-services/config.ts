983943a8339f2366459a1aa4a43348a29e51c613
Saikumar Nani <saikumarvardhi7799@gmail.com>
2024-10-23 13:42:52 +0000

    Update config.ts

--- libs/shared-services/config.ts
+++ libs/shared-services/config.ts
@@ -1,6 +1,6 @@
-const mailLiveURL = "http://139.59.79.77:3338/api/mailer";
+const mailLiveURL = "https://gatex-be.schemaxtech.in/api/mailer";
 
 export const config = {
-    APP_DC: "http://139.59.79.77:3338/api",
+    APP_DC: "https://gatex-be.schemaxtech.in/api",
     // APP_DC: "http://gpdc.seplcloud.com:3011/api",
 }
